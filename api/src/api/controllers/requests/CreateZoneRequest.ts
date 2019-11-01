8c1e925b306a209ceb49f67f1ad4cf0e3121ec29
Spurtcommerce <spurtcommerce@gmail.com>
2019-11-01 12:39:33 +0530

    Initial Commit

---
+++ api/src/api/controllers/requests/CreateZoneRequest.ts
@@ -0,0 +1,31 @@
+/*
+ * spurtcommerce API
+ * version 3.0
+ * Copyright (c) 2019 piccosoft ltd
+ * Author piccosoft ltd <support@piccosoft.com>
+ * Licensed under the MIT license.
+ */
+
+import 'reflect-metadata';
+import { IsNotEmpty , MaxLength } from 'class-validator';
+
+export class CreateZone {
+
+    @IsNotEmpty()
+    public countryId: number;
+
+    @MaxLength(30, {
+        message: 'code is maximum 30 character',
+    })
+    @IsNotEmpty()
+    public code: string;
+
+    @MaxLength(30, {
+        message: 'name is maximum 30 character',
+    })
+    @IsNotEmpty()
+    public name: string;
+
+    @IsNotEmpty()
+    public status: number;
+}
---
+++ api/src/api/controllers/requests/UpdateTodayDealsParam.ts
@@ -0,0 +1,13 @@
+/*
+ * spurtcommerce API
+ * version 3.0
+ * Copyright (c) 2019 piccosoft ltd
+ * Author piccosoft ltd <support@piccosoft.com>
+ * Licensed under the MIT license.
+ */
+
+import 'reflect-metadata';
+export class UpdateTodayDealsParam {
+
+    public todayDeals: number;
+}
